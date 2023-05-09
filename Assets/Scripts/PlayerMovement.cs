5878c2bfa42adeed3bbb65a6d6063a6f4b99cabb
BogDog4 <Bramanofficial@gmail.com>
2023-05-09 03:15:01 -0400

    added all the sprites
    and also implemented broken animations to the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,23 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class playerMovement : MonoBehaviour
 {
     private float horizontal;
     private float speed = 6f;
     private float jumpForce = 12f;
     private bool facingRight = true;
+    private bool isWalking = false;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private Transform cornerpos;
-    [SerializeField] private Transform cornerJumpLimit;
+
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("horizontal");
         
+        isWalking = Mathf.Abs(horizontal) > 0.2f; // checks if the player is moving at least .2
+
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
@@ -28,27 +30,45 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f); // small jump
         }
+        
         Flip();
     }
 
     private void FixedUpdate()
-    {//set the X component of rigidbody velocity to the horiz input times speed value (8)
-        rb.velocity = new Vector3(horizontal * speed, rb.velocity.y); 
+    {
+        //set the X component of rigidbody velocity to the horiz input times speed value (8)
+        rb.velocity = new Vector3(horizontal * speed, rb.velocity.y);
     }
 
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer); //creates a circle with radius of .2 centered on groundcheck(touchgrass) that checks if it's touching groundlayer
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
-    
+
+    public bool IsJumping()
+    {
+        bool jumping = rb.velocity.y > 0f;
+        Debug.Log("IsJumping: " + jumping);
+        return jumping;
+    }
+
+    public bool IsFalling()
+    {
+        bool falling = rb.velocity.y < 0f && !IsGrounded();
+        Debug.Log("IsFalling: " + falling);
+        return falling;
+    }
+
     private void Flip()
-    {//if your facing right and pressing left, or vice versa, flip.
-        if (facingRight && horizontal <0 || !facingRight && horizontal > 0f)
+    {
+        //if your facing right and pressing left, or vice versa, flip.
+        if (facingRight && horizontal < 0 || !facingRight && horizontal > 0f)
         {
             facingRight = !facingRight; //when flip is initiated, set facingright to true/false
             Vector3 localScale = transform.localScale;
-            localScale.x *= -1f; //
+            localScale.x *= -1f; //flip the whole player prefab backwards
             transform.localScale = localScale;
         }
     }
 }
+
