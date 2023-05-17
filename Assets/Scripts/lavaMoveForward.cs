2cc91f6539747060b2fe2057ba075cc4f3ee7f97
BogDog4 <Bramanofficial@gmail.com>
2023-05-17 12:58:37 -0400

    made lava collision work
    and the scenes change from one to another and on death

--- Assets/Scripts/lavaMoveForward.cs
+++ Assets/Scripts/lavaMoveForward.cs
@@ -5,16 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class lavaMoveForward : MonoBehaviour {
     public float speed = 1f; // speed of lava flow
+    public bool lavaIsActive;
 
     private void Update() {
         transform.Translate(Vector3.back * speed * Time.deltaTime); // move the lava block in the negative z direction
-        if(transform.position.z <= .5f) { // stop moving the lava block when it reaches -3 units in the z direction
+        if(transform.position.z <= .5f) { // stop moving the lava block when it reaches .5 in the z direction
             speed = 0f;
+            lavaIsActive = true;
         }
     }
 
-    private void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.CompareTag("Player")) {
+    private void OnTriggerStay2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player") && lavaIsActive) {
             SceneManager.LoadScene("tryAgain"); // Load the "tryAgain" scene
         }
     }
